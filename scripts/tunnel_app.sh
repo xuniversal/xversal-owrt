0f2fbfc0d97b4e2e1399a62da2fb289f49abbf38
nullvoid <77772721+xuniversal@users.noreply.github.com>
2025-01-27 09:49:58 +0700

    Update tunnel_app.sh

--- scripts/tunnel_app.sh
+++ scripts/tunnel_app.sh
@@ -14,21 +14,7 @@ neko_file_down="$(curl -s https://api.github.com/repos/nosignals/neko/releases/l
 passwall_api="https://api.github.com/repos/xiaorouji/openwrt-passwall/releases"
 passwall_file="passwall_packages_ipk_$ARCH_3.zip"
 passwall_file_down="$(curl -s ${passwall_api} | grep "browser_download_url" | grep -oE "https.*${passwall_file}" | head -n 1)"
-passwall_ipk="https://github.com/xiaorouji/openwrt-passwall/releases/download/25.1.15-1/luci-23.05_luci-app-passwall_25.1.15_all.ipk"
-
-# passwall2
-passwall2_api="https://api.github.com/repos/xiaorouji/openwrt-passwall2/releases"
-passwall2_file="passwall2_packages_ipk_$ARCH_3.zip"
-passwall2_file_down="$(curl -s ${passwall2_api} | grep "browser_download_url" | grep -oE "https.*${passwall2_file}.*.ipk" | head -n 1)"
-passwall2_ipk="https://github.com/xiaorouji/openwrt-passwall2/releases/download/25.1.8-1/luci-23.05_luci-app-passwall2_25.1.8_all.ipk"
-
-passwall_ipk_packages=("https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/dns2socks_2.1-2_$ARCH_3.ipk"
-                       "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/dns2tcp_1.1.0-1_$ARCH_3.ipk"
-                       "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/chinadns-ng_2023.06.01-1_$ARCH_3.ipk"
-                       "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/xray-plugin_1.8.4-1_$ARCH_3.ipk"
-                       "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/trojan-plus_10.0.3-2_$ARCH_3.ipk"
-                       "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/ipt2socks_1.1.3-3_$ARCH_3.ipk"
-                       "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/pdnsd-alt_1.2.9b-par-3_$ARCH_3.ipk")                
+passwall_ipk="https://github.com/xiaorouji/openwrt-passwall/releases/download/25.1.15-1/luci-23.05_luci-app-passwall_25.1.15_all.ipk"  
 
 if [ "$1" == "openclash" ]; then
     echo "Downloading Openclash packages"
