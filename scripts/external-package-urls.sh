acff61012ab5cc62fc34d4bd17fbc8c3fa9c77eb
nullvoid <77772721+xuniversal@users.noreply.github.com>
2025-02-16 13:29:32 +0700

    Update external-package-urls.sh

--- scripts/external-package-urls.sh
+++ scripts/external-package-urls.sh
@@ -17,6 +17,8 @@ files1=(
     "libmbim|https://downloads.openwrt.org/releases/packages-23.05/$ARCH_3/packages"
     "libqmi|https://downloads.openwrt.org/releases/packages-23.05/$ARCH_3/packages"
     "dns2tcp|https://downloads.immortalwrt.org/releases/packages-24.10/$ARCH_3/packages"
+    "luci-app-argon-config|https://downloads.immortalwrt.org/releases/packages-24.10/$ARCH_3/luci"
+    "luci-theme-argon|https://downloads.immortalwrt.org/releases/packages-24.10/$ARCH_3/luci"
     #"sms-tool|https://downloads.openwrt.org/releases/packages-23.05/$ARCH_3/packages"
     "luci-app-argon-config|https://fantastic-packages.github.io/packages/releases/$(echo "$BRANCH" | cut -d'.' -f1-2)/packages/$ARCH_3/luci"
     "luci-theme-argon|https://fantastic-packages.github.io/packages/releases/$(echo "$BRANCH" | cut -d'.' -f1-2)/packages/$ARCH_3/luci"
