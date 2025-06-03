4ca73f8e50c02eafd3194363c34e9f4631a4a1ce
nullvoid <77772721+xuniversal@users.noreply.github.com>
2025-06-03 11:17:46 +0700

    Update external-package-urls.sh

--- scripts/external-package-urls.sh
+++ scripts/external-package-urls.sh
@@ -70,11 +70,11 @@ fi
 
 files2+=(
     "luci-app-adguardhome|https://api.github.com/repos/kongfl888/luci-app-adguardhome/releases/latest"
-    "luci-app-sms-tool-js|https://api.github.com/repos/4IceG/luci-app-sms-tool-js/releases/latest"
+    "luci-app-sms-tool-js|https://dl.openwrt.ai/releases/24.10/packages/x86_64/kiddin9/"
     "luci-app-modemband|https://api.github.com/repos/4IceG/luci-app-modemband/releases/latest"
     "modemband|https://api.github.com/repos/4IceG/luci-app-modemband/releases/latest"
     "luci-app-lite-watchdog|https://api.github.com/repos/4IceG/luci-app-lite-watchdog/releases/latest"
-    "luci-app-3ginfo-lite|https://api.github.com/repos/4IceG/luci-app-3ginfo-lite/releases/latest"
+    "luci-app-3ginfo-lite|https://dl.openwrt.ai/releases/24.10/packages/x86_64/kiddin9/"
     "luci-app-netmonitor|https://api.github.com/repos/rtaserver/rta-packages/releases"
     "luci-app-base64|https://api.github.com/repos/rtaserver/rta-packages/releases"
     "luci-theme-alpha|https://api.github.com/repos/derisamedia/luci-theme-alpha/releases/latest"
