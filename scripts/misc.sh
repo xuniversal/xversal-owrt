3c8e0102d341cab66043c5d64a2af2850c76db92
nullvoid <77772721+xuniversal@users.noreply.github.com>
2025-06-17 12:16:32 +0700

    Update misc.sh

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -10,52 +10,52 @@ if [[ "$BASE" == "openwrt" ]]; then
     sed -i '/# setup misc settings/ a\mv \/www\/luci-static\/resources\/view\/status\/include\/29_temp.js \/www\/luci-static\/resources\/view\/status\/include\/17_temp.js' files/etc/uci-defaults/99-init-settings.sh
 elif [[ "$BASE" == "immortalwrt" ]]; then
     echo "$BASE"
-VERSION_MAJOR=$(echo "$BRANCH" | grep -oE '^[0-9]+' || echo "0")
-if [ "$VERSION_MAJOR" -ge 21 ] && [ "$PROFILE" == "rpi-4" ]; then
-    echo "Running setup for version $BRANCH and profile $PROFILE"
-    
-    cp packages/luci-app-oled_1.0_all.ipk files/root/luci-app-oled_1.0_all.ipk
+    VERSION_MAJOR=$(echo "$BRANCH" | grep -oE '^[0-9]+' || echo "0")
+    if [ "$VERSION_MAJOR" -ge 21 ] && [ "$PROFILE" == "rpi-4" ]; then
+        echo "Running setup for version $BRANCH and profile $PROFILE"
 
-    sed -i '/# setup misc settings/ a\rm /root/luci-app-oled_1.0_all.ipk' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\opkg install /root/luci-app-oled_1.0_all.ipk --force-downgrade' files/etc/uci-defaults/99-init-settings.sh
-fi
+        cp packages/luci-app-oled_1.0_all.ipk files/root/luci-app-oled_1.0_all.ipk
+        sed -i '/# setup misc settings/ a\rm /root/luci-app-oled_1.0_all.ipk' files/etc/uci-defaults/99-init-settings.sh
+        sed -i '/# setup misc settings/ a\opkg install /root/luci-app-oled_1.0_all.ipk --force-downgrade' files/etc/uci-defaults/99-init-settings.sh
+    fi
 
-if [ "$(echo "$BRANCH" | cut -d'.' -f1)" == "21" ] || [ "$TYPE" == "AMLOGIC" ] || [ "$ROOTFS_SQUASHFS" == "true" ]; then
-    rm files/etc/uci-defaults/70-rootpt-resize
-    rm files/etc/uci-defaults/80-rootfs-resize
-    rm files/etc/sysupgrade.conf
-fi
+    if [ "$(echo "$BRANCH" | cut -d'.' -f1)" == "21" ] || [ "$TYPE" == "AMLOGIC" ] || [ "$ROOTFS_SQUASHFS" == "true" ]; then
+        rm files/etc/uci-defaults/70-rootpt-resize
+        rm files/etc/uci-defaults/80-rootfs-resize
+        rm files/etc/sysupgrade.conf
+    fi
 
-# add yout custom command for specific target and release branch version here
-if [ "$(echo "$BRANCH" | cut -d'.' -f1)" == "21" ]; then
-    echo "$BRANCH"
-elif [ "$(echo "$BRANCH" | cut -d'.' -f1)" == "23" ]; then
-    echo "$BRANCH"
-fi
+    # add your custom command for specific target and release branch version here
+    if [ "$(echo "$BRANCH" | cut -d'.' -f1)" == "21" ]; then
+        echo "$BRANCH"
+    elif [ "$(echo "$BRANCH" | cut -d'.' -f1)" == "23" ]; then
+        echo "$BRANCH"
+    fi
 
-if [ "$TARGET" == "Raspberry Pi 4B" ]; then
-    echo "$TARGET"
-elif [ "$TARGET" == "x86-64" ]; then
-    rm packages/luci-app-oled_1.0_all.ipk
-else
-    rm packages/luci-app-oled_1.0_all.ipk
-fi
+    if [ "$TARGET" == "Raspberry Pi 4B" ]; then
+        echo "$TARGET"
+    elif [ "$TARGET" == "x86-64" ]; then
+        rm packages/luci-app-oled_1.0_all.ipk
+    else
+        rm packages/luci-app-oled_1.0_all.ipk
+    fi
 
-if [ "$TYPE" == "AMLOGIC" ]; then
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/dhcp-get-server.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/dhcp.script' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/dhcpv6.script' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/ppp6-up' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/ppp-down' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/ppp6-down' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/ppp-up' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/wireless/mac80211.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/proto/dhcp.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/nefitd/proto/dhcpv6.sh' files/etc/uci-defaults/99-init-settings.sh
-    sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/proto/ppp.sh' files/etc/uci-defaults/99-init-settings.sh
-else
-    rm -rf files/lib
-fi
+    if [ "$TYPE" == "AMLOGIC" ]; then
+        sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/dhcp-get-server.sh' files/etc/uci-defaults/99-init-settings.sh
+        sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/dhcp.script' files/etc/uci-defaults/99-init-settings.sh
+        sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/dhcpv6.script' files/etc/uci-defaults/99-init-settings.sh
+        sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/ppp6-up' files/etc/uci-defaults/99-init-settings.sh
+        sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/ppp-down' files/etc/uci-defaults/99-init-settings.sh
+        sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/ppp6-down' files/etc/uci-defaults/99-init-settings.sh
+        sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/ppp-up' files/etc/uci-defaults/99-init-settings.sh
+        sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/wireless/mac80211.sh' files/etc/uci-defaults/99-init-settings.sh
+        sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/proto/dhcp.sh' files/etc/uci-defaults/99-init-settings.sh
+        sed -i '/# setup misc settings/ a\chmod +x /lib/nefitd/proto/dhcpv6.sh' files/etc/uci-defaults/99-init-settings.sh
+        sed -i '/# setup misc settings/ a\chmod +x /lib/netifd/proto/ppp.sh' files/etc/uci-defaults/99-init-settings.sh
+    else
+        rm -rf files/lib
+    fi
+fi  # <--- ini adalah penutup untuk `elif [[ "$BASE" == "immortalwrt" ]]`
 
 # custom script files urls
 echo "Downloading custom script" 
