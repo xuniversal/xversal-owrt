8eaa6ab28abaa10d1fd64416977af0afabb02c77
nullvoid <77772721+xuniversal@users.noreply.github.com>
2025-07-23 22:21:48 +0700

    Update make-build.sh

--- make-build.sh
+++ make-build.sh
@@ -18,7 +18,7 @@ kmod-usb-uhci kmod-usb2 kmod-usb-ehci kmod-usb-net-ipheth usbmuxd libusbmuxd-uti
 kmod-phy-broadcom kmod-phylib-broadcom kmod-tg3"
 
 # Modem Tools
-PACKAGES+=" modeminfo luci-app-modeminfo atinout modemband luci-app-modemband luci-app-mmconfig sms-tool luci-app-sms-tool-js luci-app-lite-watchdog luci-app-3ginfo-lite picocom minicom"
+# PACKAGES+=" modeminfo luci-app-modeminfo atinout modemband luci-app-modemband luci-app-mmconfig sms-tool luci-app-sms-tool-js luci-app-lite-watchdog luci-app-3ginfo-lite picocom minicom"
 
 # Tunnel option
 OPENCLASH_FW3="coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun unzip luci-compat luci luci-base luci-app-openclash"
@@ -43,28 +43,28 @@ fi
 
 # 
 # PACKAGES+=" mihomo luci-app-mihomo"
-PACKAGES+=" nikki luci-app-nikki yq"
+# PACKAGES+=" nikki luci-app-nikki yq"
 # luci-app-passwall2
 # Adguardhome
-PACKAGES+=" luci-app-adguardhome ca-certificates ca-bundle tar unzip bind-tools"
-PACKAGES+=" tailscale luci-app-tailscale"
+# PACKAGES+=" luci-app-adguardhome ca-certificates ca-bundle tar unzip bind-tools"
+# PACKAGES+=" tailscale luci-app-tailscale"
 
 # NAS and Hard disk tools
-PACKAGES+=" luci-app-diskman luci-app-hd-idle luci-app-disks-info smartmontools kmod-usb-storage kmod-usb-storage-uas ntfs-3g"
-PACKAGES+=" samba4-server luci-app-samba4 aria2 ariang luci-app-aria2 luci-app-tinyfm"
+# PACKAGES+=" luci-app-diskman luci-app-hd-idle luci-app-disks-info smartmontools kmod-usb-storage kmod-usb-storage-uas ntfs-3g"
+# PACKAGES+=" samba4-server luci-app-samba4 aria2 ariang luci-app-aria2 luci-app-tinyfm"
 
 # Docker
 # PACKAGES+=" docker docker-compose dockerd luci-app-dockerman"
 
 # Bandwidth And Network Monitoring
-PACKAGES+=" internet-detector luci-app-internet-detector internet-detector-mod-modem-restart nlbwmon luci-app-nlbwmon vnstat2 vnstati2 luci-app-netmonitor"
+# PACKAGES+=" internet-detector luci-app-internet-detector internet-detector-mod-modem-restart nlbwmon luci-app-nlbwmon vnstat2 vnstati2 luci-app-netmonitor"
 # luci-app-vnstat2
 
 # Speedtest
-PACKAGES+=" librespeed-go python3-speedtest-cli iperf3 luci-app-netspeedtest"
+# PACKAGES+=" librespeed-go python3-speedtest-cli iperf3 luci-app-netspeedtest"
 
 # Base64 Encode Decode
-PACKAGES+=" luci-app-base64"
+# PACKAGES+=" luci-app-base64"
 
 # Argon Theme
 PACKAGES+=" luci-theme-argon luci-app-argon-config"
@@ -73,7 +73,7 @@ PACKAGES+=" luci-theme-argon luci-app-argon-config"
 PACKAGES+=" luci-theme-alpha luci-app-alpha-config"
 
 # Rakitan Manager
-PACKAGES+=" python3 python3-pip luci-app-rakitanmanager"
+# PACKAGES+=" python3 python3-pip luci-app-rakitanmanager"
 
 # Dashboard
 # PACKAGES+=" quickstart luci-app-quickstart"
